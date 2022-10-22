8b5fd4bb2d63e2c18b6f2ac1c4ca8ec57d3d2acf
Miguel Jara <mjara@cca.cl>
2022-10-21 21:57:54 -0300

    Primera versión
    Se sube primera versión de la página

---
+++ assets/js/carousel.js
@@ -0,0 +1,17 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("principal__carousel__slide");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block"; 
+}
